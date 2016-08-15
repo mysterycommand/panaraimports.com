93ed3a3676c48cb9e9dc67f9551b8fbbd3af30ab
Matt Hayes <matt@mysterycommand.com>
2016-08-14 19:43:00 -0700

    chore: move Purple906's files over the old ones

--- config/gulp/minify-images.js
+++ config/gulp/minify-images.js
@@ -4,6 +4,7 @@ import gimagemin from 'gulp-imagemin';
 export default () => {
     return gulp
         .src([
+            'source/assets/*',
             'source/images/*',
         ], {
             base: 'source',
